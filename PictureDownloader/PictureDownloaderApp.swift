b582e3c70c526d709993b2c24bda83fbee49a08c
Holger Hinzberg <holger@hinzberg.de>
2020-11-17 16:10:19 +0100

    Initial Commit

---
+++ PictureDownloader/PictureDownloaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  PictureDownloaderApp.swift
+//  PictureDownloader
+//
+//  Created by Holger Hinzberg on 17.11.20.
+//
+
+import SwiftUI
+
+@main
+struct PictureDownloaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
